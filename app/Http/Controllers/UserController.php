7e0849f8ed6f9f8be9a3093867e3b58f37ea1801
= <=>
2024-10-07 07:13:12 +0700

    ini JS 4 praktikum 1 dan 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,29 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\UserModel;
+
 use Illuminate\Http\Request;
+use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // Prepare data for a new user
-       /* $data = [
-            'username' => 'customer-1',
-            'nama' => 'pelanggan',
-            'password' => Hash::make('12345'), // Hashing the password
-            'level_id' => 4
-        ];
-        UserModel::created($data);*/
-
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1') -> update($data);
-        
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
+        return view ('user', ['data' => $user]);
     }
 }
