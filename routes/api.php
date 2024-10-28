84b604ebf11f7f8d3aa75c458e346b09346a2324
= <=>
2024-10-28 10:24:18 +0700

    JOBSHEET 10 PRAKTIKUM 3

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class) ->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class) ->name('login');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class) ->name('register');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->middleware('auth:api');
